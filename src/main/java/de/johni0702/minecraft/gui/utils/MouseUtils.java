681b42354f29b28ae1d600828251366a025e4bc5
johni0702 <me@johni0702.de>
2016-02-19 12:56:07 +0100

    Initial commit

---
+++ src/main/java/de/johni0702/minecraft/gui/utils/MouseUtils.java
@@ -0,0 +1,50 @@
+/*
+ * This file is part of jGui API, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 johni0702 <https://github.com/johni0702>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.johni0702.minecraft.gui.utils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.util.Point;
+
+public class MouseUtils {
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    public static Point getMousePos() {
+        Point scaled = getScaledDimensions();
+        int width = scaled.getX();
+        int height = scaled.getY();
+
+        int mouseX = (Mouse.getX() * width / mc.displayWidth);
+        int mouseY = (height - Mouse.getY() * height / mc.displayHeight);
+
+        return new Point(mouseX, mouseY);
+    }
+
+    public static Point getScaledDimensions() {
+        ScaledResolution res = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
+        return new Point(res.getScaledWidth(), res.getScaledHeight());
+    }
+}
---
+++ src/main/java/de/johni0702/minecraft/gui/utils/StringUtils.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of jGui API, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 johni0702 <https://github.com/johni0702>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.johni0702.minecraft.gui.utils;
+
+import net.minecraft.client.Minecraft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtils {
+    public static String[] splitStringInMultipleRows(String string, int maxWidth) {
+        if(string == null) return new String[0];
+        Minecraft mc = Minecraft.getMinecraft();
+        List<String> rows = new ArrayList<>();
+        String remaining = string;
+        while(remaining.length() > 0) {
+            String[] split = remaining.split(" ");
+            String b = "";
+            for(String sp : split) {
+                b += sp + " ";
+                if(mc.fontRendererObj.getStringWidth(b.trim()) > maxWidth) {
+                    b = b.substring(0, b.trim().length() - (sp.length()));
+                    break;
+                }
+            }
+            String trimmed = b.trim();
+            rows.add(trimmed);
+            try {
+                remaining = remaining.substring(trimmed.length() + 1);
+            } catch(Exception e) {
+                break;
+            }
+        }
+
+        return rows.toArray(new String[rows.size()]);
+    }
+}
