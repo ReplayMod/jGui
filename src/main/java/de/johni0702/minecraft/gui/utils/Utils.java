6a60f4e80a005f5dede9dbfd0d9b5fd70fd65d15
johni0702 <me@johni0702.de>
2016-03-02 20:59:38 +0100

    Add utility method for linking Focusable components of a GUI

---
+++ src/main/java/de/johni0702/minecraft/gui/utils/Utils.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of jGui API, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016 johni0702 <https://github.com/johni0702>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.johni0702.minecraft.gui.utils;
+
+import de.johni0702.minecraft.gui.function.Focusable;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class Utils {
+    /**
+     * Link the specified focusable compontents in the order they're supplied.
+     * @param focusables Focusables to link
+     * @see Focusable#setNext(Focusable)
+     * @see Focusable#setPrevious(Focusable)
+     */
+    public static void link(Focusable... focusables) {
+        checkArgument(new HashSet<>(Arrays.asList(focusables)).size() == focusables.length, "focusables must be unique and not null");
+        for (int i = 0; i < focusables.length; i++) {
+            Focusable next = focusables[(i + 1) % focusables.length];
+            focusables[i].setNext(next);
+            next.setPrevious(focusables[i]);
+        }
+    }
+}
