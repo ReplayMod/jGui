038de142e41c48e6d77e2bc965667eb8fc04a470
johni0702 <me@johni0702.de>
2016-03-04 20:00:59 +0100

    Add missing constructor to GuiScrollable

--- src/main/java/de/johni0702/minecraft/gui/container/GuiScrollable.java
+++ src/main/java/de/johni0702/minecraft/gui/container/GuiScrollable.java
@@ -25,6 +25,13 @@
 package de.johni0702.minecraft.gui.container;
 
 public class GuiScrollable extends AbstractGuiScrollable<GuiScrollable> {
+    public GuiScrollable() {
+    }
+
+    public GuiScrollable(GuiContainer container) {
+        super(container);
+    }
+
     @Override
     protected GuiScrollable getThis() {
         return this;
