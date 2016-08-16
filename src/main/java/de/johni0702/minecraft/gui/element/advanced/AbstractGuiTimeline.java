f135e29849d6e8480b49ebe622af0bb882be8240
johni0702 <me@johni0702.de>
2016-08-16 14:24:37 +0200

    Fix size not being protected in AbstractGuiTimeline

--- src/main/java/de/johni0702/minecraft/gui/element/advanced/AbstractGuiTimeline.java
+++ src/main/java/de/johni0702/minecraft/gui/element/advanced/AbstractGuiTimeline.java
@@ -58,7 +58,7 @@ public abstract class AbstractGuiTimeline<T extends AbstractGuiTimeline<T>> exte
     private boolean drawCursor = true;
     private boolean drawMarkers;
 
-    ReadableDimension size;
+    protected ReadableDimension size;
 
     public AbstractGuiTimeline() {
     }
