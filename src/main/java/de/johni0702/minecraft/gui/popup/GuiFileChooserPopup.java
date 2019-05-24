ReplayMod/jGui

commit 213ac13627178f49ce9671b29b6a542cb0e4c922

Separate layout() from draw()
Fixes IllegalStateException in convertFor when used before first render.

src/main/java/de/johni0702/minecraft/gui/popup/GuiFileChooserPopup.java
@@ -73,7 +73,6 @@ public class GuiFileChooserPopup extends AbstractGuiPopup<GuiFileChooserPopup> i
     private final GuiScrollable pathScrollable = new GuiScrollable(popup) {
         @Override
         public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
-            this.lastRenderSize = size;
             scrollX(0);
             super.draw(renderer, size, renderInfo);
         }

src/main/java/de/johni0702/minecraft/gui/container/AbstractGuiContainer.java
@@ -87,6 +87,9 @@ public abstract class AbstractGuiContainer<T extends AbstractGuiContainer<T>>
 
     @Override
     public void convertFor(GuiElement element, Point point, int relativeLayer) {
+        if (layedOutElements == null) {
+            layout(null, null);
+        }
         checkState(layedOutElements != null, "Cannot convert position unless rendered at least once.");
         Pair<ReadablePoint, ReadableDimension> pair = layedOutElements.get(element);
         checkState(pair != null, "Element " + element + " not part of " + this);
@@ -131,8 +134,9 @@ public abstract class AbstractGuiContainer<T extends AbstractGuiContainer<T>>
     }
 
     @Override
-    public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
-        super.draw(renderer, size, renderInfo);
+    public void layout(ReadableDimension size, RenderInfo renderInfo) {
+        super.layout(size, renderInfo);
+        if (size == null) return;
         try {
             layedOutElements = layout.layOut(this, size);
         } catch (Exception ex) {
@@ -143,6 +147,27 @@ public abstract class AbstractGuiContainer<T extends AbstractGuiContainer<T>>
             MCVer.addDetail(category, "Layout", layout::toString);
             throw new ReportedException(crashReport);
         }
+        for (final Map.Entry<GuiElement, Pair<ReadablePoint, ReadableDimension>> e : layedOutElements.entrySet()) {
+            GuiElement element = e.getKey();
+            if (element instanceof ComposedGuiElement) {
+                if (((ComposedGuiElement) element).getMaxLayer() < renderInfo.layer) {
+                    continue;
+                }
+            } else {
+                if (element.getLayer() != renderInfo.layer) {
+                    continue;
+                }
+            }
+            ReadablePoint ePosition = e.getValue().getLeft();
+            ReadableDimension eSize = e.getValue().getRight();
+            element.layout(eSize, renderInfo.offsetMouse(ePosition.getX(), ePosition.getY())
+                    .layer(renderInfo.getLayer() - element.getLayer()));
+        }
+    }
+
+    @Override
+    public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
+        super.draw(renderer, size, renderInfo);
         if (backgroundColor != null && renderInfo.getLayer() == 0) {
             renderer.drawRect(0, 0, size.getWidth(), size.getHeight(), backgroundColor);
         }

src/main/java/de/johni0702/minecraft/gui/container/AbstractGuiOverlay.java
@@ -134,6 +134,20 @@ public abstract class AbstractGuiOverlay<T extends AbstractGuiOverlay<T>> extend
     }
 
     @Override
+    public void layout(ReadableDimension size, RenderInfo renderInfo) {
+        if (size == null) {
+            size = screenSize;
+        }
+        super.layout(size, renderInfo);
+        if (mouseVisible && renderInfo.layer == getMaxLayer()) {
+            final GuiElement tooltip = forEach(GuiElement.class).getTooltip(renderInfo);
+            if (tooltip != null) {
+                tooltip.layout(tooltip.getMinSize(), renderInfo);
+            }
+        }
+    }
+
+    @Override
     public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
         super.draw(renderer, size, renderInfo);
         if (mouseVisible && renderInfo.layer == getMaxLayer()) {
@@ -203,8 +217,12 @@ public abstract class AbstractGuiOverlay<T extends AbstractGuiOverlay<T>> extend
                     mouseX = mouse.getX();
                     mouseY = mouse.getY();
                 }
+                RenderInfo renderInfo = new RenderInfo(MCVer.getPartialTicks(event), mouseX, mouseY, 0);
                 for (int layer = 0; layer <= layers; layer++) {
-                    draw(renderer, screenSize, new RenderInfo(MCVer.getPartialTicks(event), mouseX, mouseY, layer));
+                    layout(screenSize, renderInfo.layer(layer));
+                }
+                for (int layer = 0; layer <= layers; layer++) {
+                    draw(renderer, screenSize, renderInfo.layer(layer));
                 }
             }
         }

src/main/java/de/johni0702/minecraft/gui/container/AbstractGuiScreen.java
@@ -73,6 +73,25 @@ public abstract class AbstractGuiScreen<T extends AbstractGuiScreen<T>> extends 
     }
 
     @Override
+    public void layout(ReadableDimension size, RenderInfo renderInfo) {
+        if (size == null) {
+            size = screenSize;
+        }
+        if (renderInfo.layer == 0) {
+            if (title != null) {
+                title.layout(title.getMinSize(), renderInfo);
+            }
+        }
+        super.layout(size, renderInfo);
+        if (renderInfo.layer == getMaxLayer()) {
+            final GuiElement tooltip = forEach(GuiElement.class).getTooltip(renderInfo);
+            if (tooltip != null) {
+                tooltip.layout(tooltip.getMinSize(), renderInfo);
+            }
+        }
+    }
+
+    @Override
     public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
         if (renderInfo.layer == 0) {
             switch (background) {
@@ -168,8 +187,12 @@ public abstract class AbstractGuiScreen<T extends AbstractGuiScreen<T>> extends 
             //#endif
 
             int layers = getMaxLayer();
+            RenderInfo renderInfo = new RenderInfo(partialTicks, mouseX, mouseY, 0);
             for (int layer = 0; layer <= layers; layer++) {
-                draw(renderer, screenSize, new RenderInfo(partialTicks, mouseX, mouseY, layer));
+                layout(screenSize, renderInfo.layer(layer));
+            }
+            for (int layer = 0; layer <= layers; layer++) {
+                draw(renderer, screenSize, renderInfo.layer(layer));
             }
         }
 

src/main/java/de/johni0702/minecraft/gui/container/AbstractGuiScrollable.java
@@ -74,10 +74,22 @@ public abstract class AbstractGuiScrollable<T extends AbstractGuiScrollable<T>> 
     }
 
     @Override
+    public void layout(ReadableDimension size, RenderInfo renderInfo) {
+        if (size != null) {
+            int width = size.getWidth();
+            int height = size.getHeight();
+            lastRenderSize = size;
+            size = super.calcMinSize();
+            size = new Dimension(Math.max(width, size.getWidth()), Math.max(height, size.getHeight()));
+            renderInfo = renderInfo.offsetMouse(-offsetX, -offsetY);
+        }
+        super.layout(size, renderInfo);
+    }
+
+    @Override
     public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
         int width = size.getWidth();
         int height = size.getHeight();
-        lastRenderSize = size;
         size = super.calcMinSize();
         size = new Dimension(Math.max(width, size.getWidth()), Math.max(height, size.getHeight()));
         renderInfo = renderInfo.offsetMouse(-offsetX, -offsetY);

src/main/java/de/johni0702/minecraft/gui/element/AbstractGuiElement.java
@@ -68,10 +68,21 @@ public abstract class AbstractGuiElement<T extends AbstractGuiElement<T>> implem
     protected abstract T getThis();
 
     @Override
-    public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
+    public void layout(ReadableDimension size, RenderInfo renderInfo) {
+        if (size == null) {
+            if (getContainer() == null) {
+                throw new RuntimeException("Any top containers must implement layout(null, ...) themselves!");
+            }
+            getContainer().layout(size, renderInfo.layer(renderInfo.layer + getLayer()));
+            return;
+        }
         if (renderInfo.layer == 0) {
             lastSize = size;
         }
+    }
+
+    @Override
+    public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
     }
 
     @Override

src/main/java/de/johni0702/minecraft/gui/element/GuiElement.java
@@ -37,6 +37,7 @@ public interface GuiElement<T extends GuiElement<T>> {
     GuiContainer getContainer();
     T setContainer(GuiContainer container);
 
+    void layout(ReadableDimension size, RenderInfo renderInfo);
     void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo);
 
     ReadableDimension getMinSize();

src/main/java/de/johni0702/minecraft/gui/element/advanced/AbstractGuiColorPicker.java
@@ -73,6 +73,16 @@ public abstract class AbstractGuiColorPicker<T extends AbstractGuiColorPicker<T>
     }
 
     @Override
+    public void layout(ReadableDimension size, RenderInfo renderInfo) {
+        super.layout(size, renderInfo);
+        if (size == null) return;
+        if (renderInfo.layer == 1) {
+            ReadableDimension offsetSize = new Dimension(PICKER_SIZE, PICKER_SIZE);
+            picker.layout(offsetSize, renderInfo);
+        }
+    }
+
+    @Override
     public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
         super.draw(renderer, size, renderInfo);
         if (renderInfo.layer == 0) {

commit 80a5b2d4516c340ea19330e026e61620c171ee0f

Fix NPE in file chooser gui on windows
Regression introduced in 213ac13627178f49ce9671b29b6a542cb0e4c922

src/main/java/de/johni0702/minecraft/gui/popup/GuiFileChooserPopup.java
@@ -234,6 +234,14 @@ public class GuiFileChooserPopup extends AbstractGuiPopup<GuiFileChooserPopup> i
                 }
 
                 @Override
+                public void layout(ReadableDimension size, RenderInfo renderInfo) {
+                    super.layout(size, renderInfo);
+                    if (renderInfo.layer == 0) {
+                        skin.layout(size, renderInfo);
+                    }
+                }
+
+                @Override
                 public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
                     super.draw(renderer, size, renderInfo);
                     if (renderInfo.layer == 0) {