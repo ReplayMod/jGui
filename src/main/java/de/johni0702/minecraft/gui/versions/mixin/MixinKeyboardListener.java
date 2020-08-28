7713f2985a7c4f536ce1e677faae2e317edaf981
Jonas Herzig <me@johni0702.de>
2020-08-28 21:19:44 +0200

    Get VanillaGuiScreen keyboard events working on 1.14+
    We did not need them so far but now I have found a use for them.

--- src/main/java/de/johni0702/minecraft/gui/versions/mixin/MixinKeyboardListener.java
+++ src/main/java/de/johni0702/minecraft/gui/versions/mixin/MixinKeyboardListener.java
@@ -1,53 +1,53 @@
 //#if FABRIC>=1
 package de.johni0702.minecraft.gui.versions.mixin;
 
+import de.johni0702.minecraft.gui.versions.callbacks.KeyboardCallback;
 import net.minecraft.client.Keyboard;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.ParentElement;
 import org.spongepowered.asm.mixin.Mixin;
-/*
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-*/
 
 @Mixin(Keyboard.class)
 public class MixinKeyboardListener {
-    /* FIXME we currently don't use these but they also don't work since their target is wrapped in a lambda,
-        see MixinMouseListener for working examples
-    @Redirect(
-            method = "onKey",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ParentElement;keyPressed(III)Z")
+    @Inject(
+            method = "method_1454",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ParentElement;keyPressed(III)Z"),
+            cancellable = true
     )
-    private boolean keyPressed(ParentElement element, int keyCode, int modifiers, int scanCode) {
-        if (KeyboardCallback.EVENT.invoker().keyPressed(keyCode, modifiers, scanCode)) {
-            return true;
-        } else {
-            return element.keyPressed(keyCode, modifiers, scanCode);
+    private void keyPressed(int i, boolean[] bls, ParentElement element, int keyCode, int scanCode, int modifiers, CallbackInfo ci) {
+        if (KeyboardCallback.EVENT.invoker().keyPressed(keyCode, scanCode, modifiers)) {
+            bls[0] = true;
+            ci.cancel();
         }
     }
 
-    @Redirect(
-            method = "onKey",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ParentElement;keyReleased(III)Z")
+    @Inject(
+            method = "method_1454",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ParentElement;keyReleased(III)Z"),
+            cancellable = true
     )
-    private boolean keyReleased(ParentElement element, int keyCode, int modifiers, int scanCode) {
-        if (KeyboardCallback.EVENT.invoker().keyReleased(keyCode, modifiers, scanCode)) {
-            return true;
-        } else {
-            return element.keyReleased(keyCode, modifiers, scanCode);
+    private void keyReleased(int i, boolean[] bls, ParentElement element, int keyCode, int scanCode, int modifiers, CallbackInfo ci) {
+        if (KeyboardCallback.EVENT.invoker().keyReleased(keyCode, scanCode, modifiers)) {
+            bls[0] = true;
+            ci.cancel();
         }
     }
 
-    @Redirect(
-            method = "onKey",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/ParentElement;charTyped(CI)Z")
-    )
-    private boolean charTyped(ParentElement element, char keyChar, int modifiers) {
-        if (KeyboardCallback.EVENT.invoker().charTyped(keyChar, modifiers)) {
-            return true;
-        } else {
-            return element.charTyped(keyChar, modifiers);
+    @Inject(method = "method_1458", at = @At("HEAD"), cancellable = true)
+    private static void charTyped(Element element, int keyChar, int modifiers, CallbackInfo ci) {
+        if (KeyboardCallback.EVENT.invoker().charTyped((char) keyChar, modifiers)) {
+            ci.cancel();
         }
-    }
-    */
+   }
+
+   @Inject(method = "method_1473", at = @At("HEAD"), cancellable = true)
+   private static void charTyped(Element element, char keyChar, int modifiers, CallbackInfo ci) {
+       if (KeyboardCallback.EVENT.invoker().charTyped(keyChar, modifiers)) {
+           ci.cancel();
+       }
+   }
 }
 //#endif
