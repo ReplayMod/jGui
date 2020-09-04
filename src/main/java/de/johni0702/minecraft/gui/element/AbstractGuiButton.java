939ead86b6ea194496e216334369d872fdd13f32
Jonas Herzig <me@johni0702.de>
2020-09-04 20:52:35 +0200

    Support rendering icons on top of GuiButton instead of just text
    This has the advantage of automatically using vanilla 1.15's button style.
    
    This largely replaces `GuiTexturedButton` except where the button wasn't vanilla
    style in the first place. At the moment no such case exists but it seems not too
    unlikely which is why the GuiTexturedButton will not be deprecated.

--- src/main/java/de/johni0702/minecraft/gui/element/AbstractGuiButton.java
+++ src/main/java/de/johni0702/minecraft/gui/element/AbstractGuiButton.java
@@ -31,6 +31,7 @@ import de.johni0702.minecraft.gui.function.Clickable;
 import de.johni0702.minecraft.gui.utils.lwjgl.Dimension;
 import de.johni0702.minecraft.gui.utils.lwjgl.Point;
 import de.johni0702.minecraft.gui.utils.lwjgl.ReadableDimension;
+import de.johni0702.minecraft.gui.utils.lwjgl.ReadablePoint;
 import de.johni0702.minecraft.gui.versions.MCVer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
@@ -66,6 +67,11 @@ public abstract class AbstractGuiButton<T extends AbstractGuiButton<T>> extends 
 
     private String label;
 
+    private Identifier texture;
+    private ReadableDimension textureSize;
+    private ReadablePoint spriteUV;
+    private ReadableDimension spriteSize;
+
     public AbstractGuiButton() {
     }
 
@@ -77,7 +83,6 @@ public abstract class AbstractGuiButton<T extends AbstractGuiButton<T>> extends 
     public void draw(GuiRenderer renderer, ReadableDimension size, RenderInfo renderInfo) {
         super.draw(renderer, size, renderInfo);
 
-        renderer.bindTexture(WIDGETS_TEXTURE);
         color4f(1, 1, 1, 1);
 
         byte texture = 1;
@@ -97,16 +102,40 @@ public abstract class AbstractGuiButton<T extends AbstractGuiButton<T>> extends 
         int textureY = 46 + texture * 20;
         int halfWidth = size.getWidth() / 2;
         int secondHalfWidth = size.getWidth() - halfWidth;
+        int halfHeight = size.getHeight() / 2;
+        int secondHalfHeight = size.getHeight() - halfHeight;
 
-        renderer.drawTexturedRect(0, 0, 0, textureY, halfWidth, size.getHeight());
-        renderer.drawTexturedRect(halfWidth, 0, 200 - secondHalfWidth, textureY, secondHalfWidth, size.getHeight());
-        renderer.drawCenteredString(halfWidth, (size.getHeight() - 8) / 2, color, label, true);
+        renderer.bindTexture(WIDGETS_TEXTURE);
+        renderer.drawTexturedRect(0, 0, 0, textureY, halfWidth, halfHeight);
+        renderer.drawTexturedRect(0, halfHeight, 0, textureY + 20 - secondHalfHeight, halfWidth, secondHalfHeight);
+        renderer.drawTexturedRect(halfWidth, 0, 200 - secondHalfWidth, textureY, secondHalfWidth, halfHeight);
+        renderer.drawTexturedRect(halfWidth, halfHeight, 200 - secondHalfWidth, textureY + 20 - secondHalfHeight, secondHalfWidth, secondHalfHeight);
+
+        if (this.texture != null) {
+            renderer.bindTexture(this.texture);
+            if (spriteUV != null && textureSize != null) {
+                ReadableDimension spriteSize = this.spriteSize != null ? this.spriteSize : getMinSize();
+                renderer.drawTexturedRect(0, 0, spriteUV.getX(), spriteUV.getY(), size.getWidth(), size.getHeight(),
+                        spriteSize.getWidth(), spriteSize.getHeight(),
+                        textureSize.getWidth(), textureSize.getHeight());
+            } else {
+                renderer.drawTexturedRect(0, 0, 0, 0, size.getWidth(), size.getHeight());
+            }
+        }
+
+        if (label != null) {
+            renderer.drawCenteredString(halfWidth, (size.getHeight() - 8) / 2, color, label, true);
+        }
     }
 
     @Override
     public ReadableDimension calcMinSize() {
-        TextRenderer fontRenderer = MCVer.getFontRenderer();
-        return new Dimension(fontRenderer.getWidth(label), 20);
+        if (label != null) {
+            TextRenderer fontRenderer = MCVer.getFontRenderer();
+            return new Dimension(fontRenderer.getWidth(label), 20);
+        } else {
+            return new Dimension(0, 0);
+        }
     }
 
     @Override
@@ -161,5 +190,49 @@ public abstract class AbstractGuiButton<T extends AbstractGuiButton<T>> extends 
 
     public String getLabel() {
         return this.label;
+    }
+
+    @Override
+    public Identifier getTexture() {
+        return texture;
+    }
+
+    @Override
+    public T setTexture(Identifier texture) {
+        this.texture = texture;
+        return getThis();
+    }
+
+    @Override
+    public ReadableDimension getTextureSize() {
+        return textureSize;
+    }
+
+    @Override
+    public T setTextureSize(ReadableDimension textureSize) {
+        this.textureSize = textureSize;
+        return getThis();
+    }
+
+    @Override
+    public ReadablePoint getSpriteUV() {
+        return spriteUV;
+    }
+
+    @Override
+    public T setSpriteUV(ReadablePoint spriteUV) {
+        this.spriteUV = spriteUV;
+        return getThis();
+    }
+
+    @Override
+    public ReadableDimension getSpriteSize() {
+        return spriteSize;
+    }
+
+    @Override
+    public T setSpriteSize(ReadableDimension spriteSize) {
+        this.spriteSize = spriteSize;
+        return getThis();
     }
 }
--- src/main/java/de/johni0702/minecraft/gui/element/IGuiButton.java
+++ src/main/java/de/johni0702/minecraft/gui/element/IGuiButton.java
@@ -24,6 +24,12 @@
  */
 package de.johni0702.minecraft.gui.element;
 
+import de.johni0702.minecraft.gui.utils.lwjgl.Dimension;
+import de.johni0702.minecraft.gui.utils.lwjgl.Point;
+import de.johni0702.minecraft.gui.utils.lwjgl.ReadableDimension;
+import de.johni0702.minecraft.gui.utils.lwjgl.ReadablePoint;
+import net.minecraft.util.Identifier;
+
 //#if MC>=10904
 import net.minecraft.sound.SoundEvent;
 //#endif
@@ -38,4 +44,39 @@ public interface IGuiButton<T extends IGuiButton<T>> extends IGuiClickable<T> {
     //#endif
 
     String getLabel();
+
+    Identifier getTexture();
+    T setTexture(Identifier identifier);
+
+    ReadableDimension getTextureSize();
+    T setTextureSize(ReadableDimension size);
+    default T setTextureSize(int width, int height) {
+        return setTextureSize(new Dimension(width, height));
+    }
+    default T setTextureSize(int size) {
+        return setTextureSize(size, size);
+    }
+
+    default T setTexture(Identifier identifier, int width, int height) {
+        return setTexture(identifier).setTextureSize(width, height);
+    }
+    default T setTexture(Identifier resourceLocation, int size) {
+        return setTexture(resourceLocation, size, size);
+    }
+
+    T setSpriteUV(ReadablePoint uv);
+    ReadablePoint getSpriteUV();
+    default T setSpriteUV(int u, int v) {
+        return setSpriteUV(new Point(u, v));
+    }
+
+    T setSpriteSize(ReadableDimension size);
+    ReadableDimension getSpriteSize();
+    default T setSpriteSize(int width, int height) {
+        return setSpriteSize(new Dimension(width, height));
+    }
+
+    default T setSprite(int u, int v, int width, int height) {
+        return setSpriteUV(u, v).setSpriteSize(width, height);
+    }
 }
